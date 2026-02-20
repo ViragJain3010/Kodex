a90b18e4bc0e2f5eb2821ae9dfa63d269ac1b7b8
ViragJain3010 <viragjain3010@gmail.com>
2026-02-20 13:02:30 +0530

    test: introduce lint failure in client

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -2,5 +2,6 @@ import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
+  const unusedVar = 'trigger failure';
   return twMerge(clsx(inputs));
 }
